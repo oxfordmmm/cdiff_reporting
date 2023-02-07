7bc3be7f461a7f87ff94a77be53bca5912d8791e
aedecano <arundecano@gmail.com>
2023-02-07 14:57:03 +0000

    Added relatedness params in report generation.

--- AMR_report.py
+++ AMR_report.py
@@ -13,16 +13,18 @@ def amr_report_args(parser):
                             help='Path to QC summary TSV')
     parser.add_argument('-a', '--amr_json', required=True,
                             help='Path to AMR JSON')
+    parser.add_argument('-r', '--relatedness_tsv', required=True,
+                            help='Path to relatedness TSV')
     parser.add_argument('-o', '--output_pdf', required=False, default="sample_report.pdf",
                             help='Path to output PDF')
     return parser
 
-def amr_report(sample_tsv: str, qc_tsv: str, amr_json:str, output_pdf:str):
+def amr_report(sample_tsv: str, qc_tsv: str, amr_json:str, relatedness_tsv:str, output_pdf:str):
     #Sample name
     try:
         with open(sample_tsv) as h:
             sample = h.readlines()
-        name = ("").join(sample)
+        sample_name_str = ("").join(sample)
     except IOError as e:
         logging.error(f"Error opening sample TSV {sample_tsv}")
         logging.error(e)
@@ -34,12 +36,22 @@ def amr_report(sample_tsv: str, qc_tsv: str, amr_json:str, output_pdf:str):
     try:
         with open(qc_tsv) as g:
             lines = g.readlines()
-        tsv = ("").join(lines)
+        qc_tsv_str = ("").join(lines)
     except IOError as e:
         logging.error(f"Error opening QC TSV {qc_tsv}")
         logging.error(e)
         exit(1)
     #print(tsv)
+
+    #Read in relatedness TSV
+    try:
+        with open(relatedness_tsv) as g:
+            lines = g.readlines()
+        relatedness_tsv_str = ("").join(lines)
+    except IOError as e:
+        logging.error(f"Error opening Relatedness TSV {relatedness_tsv}")
+        logging.error(e)
+        exit(1)
 
     #Read in AMR data in JSON
     try:
@@ -76,7 +88,7 @@ def amr_report(sample_tsv: str, qc_tsv: str, amr_json:str, output_pdf:str):
 
     pdf.set_font("Times", size=14)
     pdf.set_text_color(0,0,0)
-    pdf.multi_cell(w=200, h=5, txt = name)
+    pdf.multi_cell(w=200, h=5, txt = sample_name_str)
     pdf.ln(10)
 
 #Append QC stats
@@ -86,7 +98,7 @@ def amr_report(sample_tsv: str, qc_tsv: str, amr_json:str, output_pdf:str):
     
     pdf.set_font("Times", size=14)
     pdf.set_text_color(0,0,0)
-    pdf.multi_cell(w=200, h=5, txt = tsv)
+    pdf.multi_cell(w=200, h=5, txt = qc_tsv_str)
     pdf.ln(10)
 
 #Append AMR Profile
@@ -114,7 +126,17 @@ def amr_report(sample_tsv: str, qc_tsv: str, amr_json:str, output_pdf:str):
         pdf.cell(pdf.font_size * 3, row_height, str(data_df['resistance'][row]), border="T", ln=0)
         pdf.cell((4*(epw/6)) - (pdf.font_size*3), row_height, str(data_df['evidence_resistance'][row]), border="TR", ln=1)
         pdf.cell(epw, row_height, str(data_df['evidence_sensitive'][row]), border="BLR", ln=1)
-    
+        
+        
+#Append Related samples section
+    pdf.ln(10)
+    pdf.set_font("Times", "B", size=14)
+    pdf.set_text_color(0,0,200) #blue
+    pdf.cell(w=0, h=5, txt="Related samples", align = "L", ln=2)
+    pdf.set_font("Times", size=14)
+    pdf.ln(10)
+
+
 #Save to PDF
     try:
         pdf.output(output_pdf)
@@ -133,5 +155,5 @@ if __name__ == '__main__':
         format='%(asctime)s - %(levelname)s - %(message)s', 
         level=logging.INFO)
 
-    amr_report(args.sample_tsv, args.qc_tsv, args.amr_json, args.output_pdf)
+    amr_report(args.sample_tsv, args.qc_tsv, args.amr_json, args.relatedness_tsv, args.output_pdf)
 
