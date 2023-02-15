c707936d7bf7d2c9a536954d6b30bb2a28ffd4a9
aedecano <arundecano@gmail.com>
2023-02-15 13:33:25 +0000

    modified format of sample.tsv and QC table output

--- 0_get_sample_name_MLST.pl
+++ 0_get_sample_name_MLST.pl
@@ -19,10 +19,14 @@ while(<in>){
     for $n (0..$#fname){}
     $fname[0] =~ s/\_R\_contigs\.fa//g;
     #print "Sample details\n";
-    print sample "Specimen identifier:\t$fname[0]\n";
+    #print sample "Specimen identifier:\t$fname[0]\n";
     #print "Sequence GUUID:\t\t$fname[1]\t\tSequencing date:\t\t$date\n";
-    print sample "Report date:\t$date\n";
-    print sample "Multi-locus sequence type:\t$fname[2]\n";
+    #print sample "Report date:\t$date\n";
+    #print sample "Multi-locus sequence type:\t$fname[2]\n";
+    print "\nReport date\tSpecimen identifier\tMulti-locus sequence type\n\n";
+    print "$date\t$fname[0]\t$fname[2]\n";
+    print sample "Report date\tSpecimen identifier\tMulti-locus sequence type\n";
+    print sample "$date\t$fname[0]\t$fname[2]\n";
 }
 
 close(in);
