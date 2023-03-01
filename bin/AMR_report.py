c576212b0fe198d33688704ec186f3f7f20b84ee
aedecano <arundecano@gmail.com>
2023-03-01 13:58:37 +0000

    revision of fonts and columns

--- bin/AMR_report.py
+++ bin/AMR_report.py
@@ -42,18 +42,18 @@ def amr_report(sample_tsv: str, qc_tsv: str, amr_json:str, relatedness_tsv:str, 
     pdf.ln(30)  # move 30 down
 
     #Show poject title
-    pdf.set_font("Times", "B", size=17)
+    pdf.set_font("Helvetica", "B", size=17)
     pdf.ln(10)  # move 10 down
     pdf.cell(w=0, h=5, txt="Clostridioides difficile Sequence Analysis Report", align = "L", ln=2)
     pdf.ln(5)
 
     #Show sample details
-    pdf.set_font("Times", "B", size=14)
+    pdf.set_font("Helvetica", "B", size=12)
     pdf.set_text_color(0,0,200) #blue
     pdf.cell(w=0, h=5, txt="Sample details", align = "L", ln=2)
     pdf.ln(1)
 
-    #pdf.set_font("Times", size=14)
+    #pdf.set_font("Helvetica", size=12)
     pdf.set_text_color(0,0,0)
 
     try:
@@ -65,15 +65,17 @@ def amr_report(sample_tsv: str, qc_tsv: str, amr_json:str, relatedness_tsv:str, 
                     line.remove("")
                 
                 if i == 0:
-                    pdf.set_font("Times", "B", size=14)
+                    pdf.set_font("Helvetica", "B", size=12)
                     pdf.cell(w=40, h=5, txt = line[0], border="TBL")
                     pdf.cell(w=60, h=5, txt = line[1], border="TB")
-                    pdf.cell(w=60, h=5, txt = line[2], border="TBR", ln=1)
+                    pdf.cell(w=60, h=5, txt = line[2], border="TB", ln=1)
+            
                 else:
-                    pdf.set_font("Times", size=14)
+                    pdf.set_font("Helvetica", size=12)
                     pdf.cell(w=40, h=5, txt = line[0], border="TBL")
                     pdf.cell(w=60, h=5, txt = line[1], border="TB")
-                    pdf.cell(w=60, h=5, txt = line[2], border="TBR", ln=1)
+                    pdf.cell(w=60, h=5, txt = line[2], border="TB", ln=1)
+                    
             pdf.ln(10)
     except Exception as e:
         logging.error(f"Error opening sample TSV {sample_tsv}")
@@ -81,7 +83,7 @@ def amr_report(sample_tsv: str, qc_tsv: str, amr_json:str, relatedness_tsv:str, 
         exit(1)
 
     #Append QC stats
-    pdf.set_font("Times", "B", size=14)
+    pdf.set_font("Helvetica", "B", size=12)
     pdf.set_text_color(0,0,200) #blue
     pdf.cell(w=0, h=5, txt="Sequencing Quality Stats", align = "L", ln=2)
     
@@ -98,7 +100,7 @@ def amr_report(sample_tsv: str, qc_tsv: str, amr_json:str, relatedness_tsv:str, 
                 pdf.cell(w=40, h=5, txt = line[1], border="TB")
                 pdf.cell(w=30, h=5, txt = line[2], border="TB")
                 pdf.cell(w=30, h=5, txt = line[3], border="TBR", ln=1)
-                pdf.set_font("Times", size=14)
+                pdf.set_font("Helvetica", size=12)
     except IOError as e:
         logging.error(f"Error opening QC TSV {qc_tsv}")
         logging.error(e)
@@ -107,23 +109,23 @@ def amr_report(sample_tsv: str, qc_tsv: str, amr_json:str, relatedness_tsv:str, 
     pdf.ln(10)
 
     #Append AMR Profile
-    pdf.set_font("Times", "B", size=14)
+    pdf.set_font("Helvetica", "B", size=12)
     pdf.set_text_color(0,0,200) #blue
     pdf.cell(w=0, h=5, txt="Antimicrobial Resistance Profile", align = "L", ln=1)
-    pdf.set_font("Times", size=14)
+    pdf.set_font("Helvetica", size=12)
 
-    pdf.set_font("Times", size=14)
+    pdf.set_font("Helvetica", size=12)
     pdf.set_text_color(0,0,0)
     epw = pdf.w - 2*pdf.l_margin
  
     row_height = pdf.font_size
-    pdf.set_font("Times", "B", size=14)
+    pdf.set_font("Helvetica", "B", size=12)
     pdf.cell(epw/3, row_height, "Drug", border="TL", ln=0)
     pdf.cell(pdf.font_size * 3, row_height, "S/R", border="T", ln=0)
     pdf.cell((4*(epw/6)) - (pdf.font_size*3), row_height, "Evidence of Resistance", border="TR", ln=1)
     pdf.cell(epw, row_height, "Catalogue features not found", border="BLR", ln=1)
 
-    pdf.set_font("Times", size=14)
+    pdf.set_font("Helvetica", size=12)
     for row in data_df.index:
         pdf.cell(epw/3, row_height, str(row), border="TL", ln=0)
         pdf.cell(pdf.font_size * 3, row_height, str(data_df['resistance'][row]), border="T", ln=0)
@@ -132,7 +134,7 @@ def amr_report(sample_tsv: str, qc_tsv: str, amr_json:str, relatedness_tsv:str, 
 
     #Append Related samples section
     pdf.ln(10)
-    pdf.set_font("Times", "B", size=14)
+    pdf.set_font("Helvetica", "B", size=12)
     pdf.set_text_color(0,0,200) #blue
     pdf.cell(w=0, h=5, txt="Related samples", align = "L", ln=2)
 
@@ -153,7 +155,7 @@ def amr_report(sample_tsv: str, qc_tsv: str, amr_json:str, relatedness_tsv:str, 
                 pdf.cell(w=40 , h=5, txt = line[1], border="TB")
                 pdf.cell(w=30 , h=5, txt = line[2], border="TB")
                 pdf.cell(w=30 , h=5, txt = line[3], border="TBR", ln=1)
-                pdf.set_font("Times", size=14)
+                pdf.set_font("Helvetica", size=12)
     except IOError as e:
         logging.error(f"Error opening Relatedness TSV {relatedness_tsv}")
         logging.error(e)
